ce1dfb064fa3de5af2c745c417b2cf2883e056a0
wagner <wagnerrodrio.pan@gmail.com>
2019-11-29 21:20:27 -0300

    Criação dos metodos buscartodos, remover,buscar e alteração do metodo persistir

--- WagnerLPWD/src/main/java/br/cesjf/wagnerlpwd/dao/QuestaoDAO.java
+++ WagnerLPWD/src/main/java/br/cesjf/wagnerlpwd/dao/QuestaoDAO.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package br.cesjf.wagnerlpwd.dao;
+import br.cesjf.wagnerlpwd.model.Questao;
 import br.cesjf.wagnerlpwd.util.PersistenceUtil;
 import java.util.ArrayList;
 import java.util.List;
@@ -25,6 +26,59 @@ public class QuestaoDAO {
         return questaoDao;
     }
     
+     public List<Questao> buscarTodas() {
+        EntityManager em = PersistenceUtil.getEntityManager();
+        Query query = em.createQuery("SELECT q FROM Questao q");
+        return query.getResultList();
+    }
+     
+     
+ public Questao persistir(Questao questaoP) {
+        EntityManager em = PersistenceUtil.getEntityManager();
+        em.getTransaction().begin();
+        try {
+            questaoP = em.merge(questaoP);
+            em.getTransaction().commit();
+            System.out.println("Registro TbAssunto gravado com sucesso");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return questaoP;
+    }
+ 
+ 
+    public String remover(Questao quest){
+        try {
+            EntityManager em = PersistenceUtil.getEntityManager();
+            em.getTransaction().begin();
+            quest = em.merge(quest);
+            em.remove(quest);
+            em.getTransaction().commit();
+            Logger.getLogger(PersistenceUtil.class.getName()).log(Level.INFO,"Prova Removidos com sucesso!!!");
+            return "Prova" + quest.getQuestaoPK()+ "Removido com Sucesso";
+        } catch (Exception e) {
+            Logger.getLogger(PersistenceUtil.class.getName()).log(Level.WARNING,"Não foi possivel remover a prova", e.getMessage());
+            return "Desculpe não foi possivel remover a prova! entre em contato com o suporte" + quest.getQuestaoPK()+"";
+        }
+    }
     
-//    public Questao buscar()
+    
+     public Questao busca(int id){
+        try {
+            EntityManager em = PersistenceUtil.getEntityManager();
+            Query query = em.createQuery("SELECT q FROM Questao q WHERE q.questaoPK.provaIdProva = :provaIdProva");
+            query.setParameter("idProva", id);
+            Questao questao = (Questao) query.getSingleResult();
+            if(questao != null && questao.getIdRespostaFk() > 0){
+                return questao;
+            }else{
+                Logger.getLogger(PersistenceUtil.class.getName()).log(Level.INFO,"Não foi encotrado o id da prova");
+                return null;
+            }
+        } catch (Exception e) {
+            Logger.getLogger(PersistenceUtil.class.getName()).log(Level.WARNING,"Erro do id prova");
+            return null;
+        }
+    }
+
 }
